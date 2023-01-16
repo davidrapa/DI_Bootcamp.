f020168da2f55f05b88c5d6800bddd3ab094a0c2
davidrapa <davidrapaport01@gmail.com>
2023-01-16 22:21:54 +0200

    vaaa

---
+++ Week3/Day2/ExcerciseXP/script.js
@@ -0,0 +1,55 @@
+// // Excercise 1
+// let favFood = "Asado"
+// let favMeal = "Dinner"
+
+// console.log(`I eat ${favFood} at every ${favMeal}`)
+
+// Excercise 2
+
+// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+// let myWatchedSeriesLength = myWatchedSeries.length
+//  let myWatchedSeriesSentence = myWatchedSeries.toString()
+
+//  console.log(`I watched ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}`)
+
+//  myWatchedSeries.splice(2, 2, "friends")
+
+// myWatchedSeries.push("breaking bad")
+
+// myWatchedSeries.unshift("Billions")
+
+// myWatchedSeries.splice(1,1)
+
+// console.log(myWatchedSeries[1][2])
+
+
+// console.log(myWatchedSeries)
+
+// // Excerise 3
+
+// let tempInCelcius = 25
+
+// let temFarenh = tempInCelcius/5*9+32
+
+// console.log(`temperature in °C ${tempInCelcius} is ${temFarenh} °F`)
+
+//Exercise 4 
+
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction: sum
+// Actual: ok
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction: sum because we asign a new content to the variable
+// Actual:ok
+
+// c is create but not declared
+
+// console.log(3 + 4 + '5');  is 75 because it sum the first 2 expresions 3+4=7 and they add a 5 like a text.
