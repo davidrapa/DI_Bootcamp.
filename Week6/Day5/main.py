53d62a0fe309d3783798c2674f7586578e23f078
davidrapa <davidrapaport01@gmail.com>
2023-02-11 21:27:33 +0200

    'W6'

---
+++ Week6/Day5/main.py
@@ -0,0 +1,15 @@
+# print(sample_dict['class']['student']['marks']['history'])
+
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+
+}
+for item in sample_dict:
+  del.sample_dict ["name"]["salary"]
+
+# del sample_dict["name"]
+# del sample_dict["salary"]
+print(sample_dict)
---
+++ Week6/Day6/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,24 @@
+# Instructions
+# Challenge 1
+# Ask a user for a word
+# Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
+#
+# Make sure the letters are the keys.
+# Make sure the letters are strings.
+# Make sure the indexes are stored in a list and those lists are values.
+# Examples
+#
+# "dodo" ➞ { "d": [0, 2], "o": [1, 3] }
+#
+# "froggy" ➞ { "f":  [0], "r": [1], "o": [2], "g": [3, 4], "y": [5] }
+#
+# "grapes" ➞ { "g": [0], "r": [1], "a": [2], "p": [3]}
+
+word = input("Enter a word: ")
+index_dict = {}
+for index, char in enumerate(word):
+    if char in index_dict:
+        index_dict[char].append(index)
+    else:
+        index_dict[char] = [index]
+print(index_dict)
