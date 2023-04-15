db71902646166a2abbd3ba3af2778cde9f2ace2b
davidrapa <davidrapaport01@gmail.com>
2023-04-15 21:38:40 +0300

    hackaton2

--- Hackaton2/fake_data_generator/app.py
+++ Hackaton2/fake_data_generator/app.py
@@ -3,13 +3,13 @@ import psycopg2
 from faker import Faker
 import json
 
-# Create the Flask application
+
 app = Flask(__name__)
 
-# Create the Faker object
+
 fake = Faker()
 
-# Set up the database connection
+# set un data
 db = psycopg2.connect(
     host="localhost",
     port="5432",
@@ -18,19 +18,19 @@ db = psycopg2.connect(
     password="1234"
 )
 
-# Define the home page route
+#  home page route
 @app.route("/")
 def home():
     return render_template("home.html")
 
-# Define the generate data route
+# generate data route
 @app.route("/generate_data", methods=["POST"])
 def generate_data():
     # Get the form data from the request
     data_type = request.form["data_type"]
     num_records = int(request.form["num_records"])
 
-    # Generate the fake data
+    #  fake data Generated item
     fake_data = []
     for i in range(num_records):
         if data_type == "name":
@@ -42,27 +42,27 @@ def generate_data():
         elif data_type == "email":
             fake_data.append(fake.email())
 
-    # Store the fake data in the database
+
     cursor = db.cursor()
     for item in fake_data:
         cursor.execute("INSERT INTO fake_data (data_type, fake_data) VALUES (%s, %s)", (data_type, item))
     db.commit()
 
-    # Render the results page with the generated data
+
     return render_template("results.html", data=fake_data)
 
-# Define the retrieve data route
+
 @app.route("/retrieve_data", methods=["GET"])
 def retrieve_data():
-    # Retrieve the fake data from the database
+
     cursor = db.cursor()
     cursor.execute("SELECT fake_data FROM fake_data")
     results = cursor.fetchall()
 
-    # Convert the results to a list of strings
+
     data = [result[0] for result in results]
 
-    # Return the data as JSON
+
     return json.dumps(data)
 
 if __name__ == "__main__":
